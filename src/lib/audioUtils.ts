71f255794d2040e114d591555597177efb64f824
Sebastian Gilits <sep.gil@gmail.com>
2023-03-18 22:39:43 +0100

    Try to fix distorted sound

--- src/lib/audioUtils.ts
+++ src/lib/audioUtils.ts
@@ -1,4 +1,6 @@
 // Prompt: Let's create a utility file, that allows me to play a tone based on a given midi note number. The tone should sound similar to a kalimba.
+// Prompt: Nice that worked. Now let's fix an issue with the sound: It sounds very distorted, but I want a clean sound, even if multiple notes are played at once.
+// GPT4: The distortion might be caused by overlapping notes with high gain. To fix this issue, we can adjust the `PolySynth` settings to have a lower gain and smoother sound. Let's update the `audioUtils.ts` file to make these changes:
 
 // File: src/lib/audioUtils.ts
 import * as Tone from 'tone';
@@ -10,7 +12,7 @@ const synth = new Tone.PolySynth(Tone.Synth, {
   envelope: {
     attack: 0.01,
     decay: 1,
-    sustain: 0,
+    sustain: 0.1,
     release: 1,
   },
 }).toDestination();
@@ -23,3 +25,5 @@ export const playNote = (midiNote: number) => {
 // Usage Example
 // import { playNote } from "@/lib/audioUtils";
 // playNote(60); // Plays a C4 note
+
+// GPT4: Now the sound should be cleaner and less distorted when multiple notes are played simultaneously.
