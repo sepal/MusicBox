3be887dc0a4c85b748ddbe1e85a184f97c3f7f16
Sebastian Gilits <sep.gil@gmail.com>
2023-03-18 14:51:45 +0100

    Fix grid playing wrong notes

--- src/components/MusicBox.tsx
+++ src/components/MusicBox.tsx
@@ -1,4 +1,5 @@
 // Prompt: Create a component that plays note based on which tile was clicked. Use a major scale, with the lowest row playing a c3 and the highest a c4. It should also have a state that remembers, which note is active based on the tiles that are active on the grid.
+// Prompt: Ok, I think I explained it not precise enough. The y coordinate in the grid should define which note to play, ignore the x coordinate for now.
 
 // File: src/components/MusicBox.tsx
 import React, { useState } from 'react';
@@ -16,7 +17,7 @@ const MusicBox: React.FC = () => {
   const [activeNotes, setActiveNotes] = useState<ActiveNotes>({});
 
   const handleTileClick = (x: number, y: number, isActive: boolean) => {
-    const note = baseNote + majorScale[x] + y * 12;
+    const note = baseNote + majorScale[y];
     setActiveNotes({ ...activeNotes, [`${x}-${y}`]: isActive });
 
     if (isActive) {
