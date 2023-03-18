740938af8299efa71e6894c906f2daef3e43379e
Sebastian Gilits <sep.gil@gmail.com>
2023-03-18 14:39:05 +0100

    Implement basic MusicBox component.

---
+++ src/components/MusicBox.tsx
@@ -0,0 +1,37 @@
+// Prompt: Create a component that plays note based on which tile was clicked. Use a major scale, with the lowest row playing a c3 and the highest a c4. It should also have a state that remembers, which note is active based on the tiles that are active on the grid.
+
+// File: src/components/MusicBox.tsx
+import React, { useState } from 'react';
+import Grid from '@/components/Grid';
+import { playNote } from '@/lib/audioUtils';
+
+const majorScale = [0, 2, 4, 5, 7, 9, 11, 12];
+const baseNote = 48; // C3
+
+interface ActiveNotes {
+  [key: string]: boolean;
+}
+
+const MusicBox: React.FC = () => {
+  const [activeNotes, setActiveNotes] = useState<ActiveNotes>({});
+
+  const handleTileClick = (x: number, y: number, isActive: boolean) => {
+    const note = baseNote + majorScale[x] + y * 12;
+    setActiveNotes({ ...activeNotes, [`${x}-${y}`]: isActive });
+
+    if (isActive) {
+      playNote(note);
+    }
+  };
+
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Grid onTileClick={handleTileClick} />
+    </div>
+  );
+};
+
+export default MusicBox;
+
+// Usage Example
+// import MusicBox from "@/components/MusicBox.tsx";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import Grid from "@/components/Grid"
-import { playNote } from "@/lib/audioUtils"
+import MusicBox from "@/components/MusicBox";
 
 export default function Home() {
   return (
     <>
-      <Grid onTileClick={(x, y, isActive) => {
-        console.log(`${x}:${y} ${isActive ? "on" : "off"}`)
-        playNote(60);
-      }} />
+      <MusicBox />
     </>
   )
 }
